5dd4090a7f43d944a484772e4e6e26d2fbd0ee14
McspeedBros <rivenstrikes@gmail.com>
2023-02-02 22:37:28 +0100

    Update connectie spreadsheet

--- src/js/overzicht.js
+++ src/js/overzicht.js
@@ -2,6 +2,7 @@ var cart = JSON.parse(window.localStorage.getItem("cart")) || [];
 var cartTableBody = document.getElementById("cart-table-body");
 var totalCost = document.getElementById("total-cost");
 var total = 0;
+var submitbutton = document.getElementById("submitbutton");
 for (var i = 0; i < cart.length; i++) {
   (function(product) {
     var tr = document.createElement("tr");
@@ -25,11 +26,6 @@ for (var i = 0; i < cart.length; i++) {
     var productPriceTd = document.createElement("td");
     productPriceTd.textContent = product.price;
     tr.appendChild(productPriceTd);
-
-    // var productTotalTd = document.createElement("td");
-    // productTotalTd.textContent =
-    //   parseFloat(product.quantity) * parseFloat(product.price.substring(1));
-    // tr.appendChild(productTotalTd);
 
     total += parseFloat(product.quantity) * parseFloat(product.price.substring(1));
 
@@ -60,6 +56,55 @@ for (var i = 0; i < cart.length; i++) {
       totalCost.textContent = "Totale prijs: € " + total;
     });
   })(cart[i]);
-}
+} 
 
 totalCost.textContent = "Totale prijs: € " + total;
+
+var submitButton = document.getElementById("submitbutton");
+ submitButton.addEventListener("click", async function(e) {
+    e.preventDefault();
+    // Alle velden moeten ingevuld zijn
+    if (document.getElementById("name").value == "" || document.getElementById("email").value == "" || document.getElementById("phone").value == "" || document.getElementById("address").value == "") {
+      alert("Vul alle correct velden in");
+      return;
+    }
+    await sendDataToGoogleSheets();
+    window.open("bevestiging.html", "_self");
+ });
+
+async function sendDataToGoogleSheets() {
+  var cart = JSON.parse(window.localStorage.getItem("cart")) || [];
+  var data = [];
+  for (var i = 0; i < cart.length; i++) {
+    data.push([
+      cart[i].name,
+      cart[i].quantity,
+      cart[i].price
+    ]);
+  }
+
+  // Send the data to Google Sheets API
+  var url = "https://sheetdb.io/api/v1/ytdi2i160aaco";
+  var result = await fetch(url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      data: [
+        {
+          "Naam": document.getElementById("name").value,
+          "E-mail": document.getElementById("email").value,
+          "Telefoonnummer": document.getElementById("phone").value,
+          "Levering": document.getElementById("leveren").value,
+          "Ophalen": document.getElementById("ophalen").value,
+          "Adres/Ophaaluur": document.getElementById("address").value,
+          "Opmerking":  document.getElementById("opmerking").value,
+          "Product": data
+
+        }
+      ]
+    })
+  });
+  console.log(result.status);
+}
